2c0a32047392cdd2855ec54ecde10e1fdee988b8
hasanbakiir <62722774+hasanbakiir@users.noreply.github.com>
2021-06-11 14:22:45 +0200

    Update MyNotFound.js

--- src/components/MyNotFound.js
+++ src/components/MyNotFound.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import MyNotFound from '../images/MyNotFound.jpg';
+import myNotFound from '../images/my.jpg';
 
-const myNotFound = () => (
+const MyNotFound = () => (
   <div>
     <h1 className="not-found-title">
-      These are not the books you are looking for...
+
     </h1>
     <figure className="not-found-img">
-      <img src={MyNotFound} alt="Page Not Found" />
+      <img src={myNotFound} alt="Page Not Found" />
       <figcaption>
-        Photo by Hope House Press - Leather Diary Studio on Unsplash
+
       </figcaption>
     </figure>
     <div className="home-link">
